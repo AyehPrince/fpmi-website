417925d401fcab7115c53cd3ec690613ddd662a3
AyehPrince <flavacrackh123@gmail.com>
2026-07-07 23:08:18 +0000

    metadata verifications

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,10 +9,13 @@ export const metadata = {
   title: "Flash Prime Media Institute | Easy Way To Excellence",
   description: "Ghana's premier media and creative arts institute in Accra. Study Broadcast Journalism, Film, Graphic Design, Fashion Design, Cosmetology and more.",
   keywords: "media school Ghana, broadcast journalism Accra, film school Ghana, FPMI, Flash Prime Media Institute, fashion design school Accra, graphic design Ghana",
+  verification: {
+    google: "0CZaai3hRJH3KPI",
+  },
   openGraph: {
     title: "Flash Prime Media Institute | Easy Way To Excellence",
     description: "Ghana's premier media and creative arts institute in Accra.",
-    url: "https://flashprimemediainstitute.com",
+    url: "https://flashmediainstitute.com",
     siteName: "Flash Prime Media Institute",
     images: [{ url: "/logo.png" }],
     locale: "en_GH",
